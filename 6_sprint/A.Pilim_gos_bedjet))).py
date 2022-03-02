d13a19e8add4736a3e5a674c09bc67b0dc9344d0
ivasnev <ivasnev2002@gmail.com>
2022-03-02 23:11:26 +0300

    fin ver 6 sprint

--- 6_sprint/A.Pilim_gos_bedjet))).py
+++ 6_sprint/A.Pilim_gos_bedjet))).py
@@ -1,4 +1,4 @@
-# ID 65578535
+# ID 65681787
 # Временная сложность O(E⋅logV), где E — количество рёбер в графе, а V — количество вершин
 # Скорость поиска max O(1) благодаря приор. очереди. шагов требуется по кол рёбер
 # Сложность по памяти мы имеем 2 сета в сумме дающие V элементов, матрицу связи с весами размера V^2,
@@ -10,40 +10,45 @@ from queue import PriorityQueue
 
 
 def input_data():
-    n, m = map(int, input().split())
-    weight = [[None for y in range(n)] for x in range(n)]
-    for i in range(m):
+    n_f, m_f = map(int, input().split())
+    weight_f = [[None for _ in range(n_f)] for _ in range(n_f)]
+    for i in range(m_f):
         a, b, c = map(int, input().split())
-        if weight[a - 1][b - 1] is None or -1 * weight[a - 1][b - 1] < c:
-            weight[a - 1][b - 1] = -c
-            weight[b - 1][a - 1] = -c
-    return n, m, weight
+        if weight_f[a - 1][b - 1] is None or -1 * weight_f[a - 1][b - 1] < c:
+            weight_f[a - 1][b - 1] = -c
+            weight_f[b - 1][a - 1] = -c
+    return n_f, m_f, weight_f
 
 
-def add_vertex(start, edges, weight):
-    added.add(start)
-    not_added.remove(start)
-    for e in [[weight[start][v], v] for v in range(n) if weight[start][v] is not None and v in not_added]:
-        edges.put(e)
+def add_vertex(start, edges_f, weight_f, added_f, not_added_f, n_f):
+    added_f.add(start)
+    not_added_f.remove(start)
+    for e_f in [[weight_f[start][v_f], v_f] for v_f in range(n_f) if
+                weight_f[start][v_f] is not None and v_f in not_added_f]:
+        edges_f.put(e_f)
 
 
-def extract_maximum(edges):
-    return edges.get()
+def extract_maximum(edges_f):
+    return edges_f.get()
 
 
-n, m, weight = input_data()
-maximum_spanning_tree = []
-added = set()
-not_added = set([x for x in range(n)])
-edges = PriorityQueue()
-v = 0
-add_vertex(v, edges, weight)
-while len(not_added) != 0 and not edges.empty():
-    e = extract_maximum(edges)
-    if e[1] in not_added:
-        maximum_spanning_tree.append(e[0])
-        add_vertex(e[1], edges, weight)
-if len(not_added) > 0:
-    print("Oops! I did it again")
-else:
-    print(-1 * sum(maximum_spanning_tree))
+def main():
+    n, m, weight = input_data()
+    maximum_spanning_tree = []
+    added = set()
+    not_added = set([x for x in range(n)])
+    edges = PriorityQueue()
+    v = 0
+    add_vertex(v, edges, weight, added, not_added, n)
+    while len(not_added) != 0 and not edges.empty():
+        e = extract_maximum(edges)
+        if e[1] in not_added:
+            maximum_spanning_tree.append(e[0])
+            add_vertex(e[1], edges, weight, added, not_added, n)
+    if len(not_added) > 0:
+        print("Oops! I did it again")
+    else:
+        print(-1 * sum(maximum_spanning_tree))
+
+
+main()
--- 6_sprint/B.Roads_in_X_town.py
+++ 6_sprint/B.Roads_in_X_town.py
@@ -1,42 +1,49 @@
-# id 65579141
-# Временная сложность O(log(V)(E+V)), где E — количество рёбер в графе, а V — количество вершин
+# id 65682114
+# Временная сложность O(E+V), где E — количество рёбер в графе, а V — количество вершин
 # так как мы используем DFS для поиска цикла
 # Сложность по памяти равна O(E) так как мы храним только рёбра
 #
-from math import log2
 
-def find_cycl_dfs(s):
-    flag = False
-    color[s] = 1
-    for i in arr[s]:
-        if color[i] == 0:
-            flag = find_cycl_dfs(i)
-        elif color[i] == 1:
+def find_cycl_dfs(s, color_f, arr_f):
+    flag_f = False
+    color_f[s] = 1
+    for i in arr_f[s]:
+        if color_f[i] == 0:
+            flag_f = find_cycl_dfs(i, color_f, arr_f)
+        elif color_f[i] == 1:
             return True
-    color[s] = 2
-    return flag
+    color_f[s] = 2
+    return flag_f
 
 
-n = int(input())
-color = [0 for i in range(n)]
-arr = [[] for x in range(n)]
-for y in range(0, n - 1):
-    str = input()
-    c = len(str) - 1
-    for x in range(n - 1, y, -1):
-        if str[c] == "R":
-            arr[y].append(x)
-        else:
-            arr[x].append(y)
-        c -= 1
+def input_gr():
+    n_f = int(input())
+    color_f = [0 for _ in range(n_f)]
+    arr_f = [[] for _ in range(n_f)]
+    for y in range(0, n_f - 1):
+        str_f = input()
+        c = len(str_f) - 1
+        for x in range(n_f - 1, y, -1):
+            if str_f[c] == "R":
+                arr_f[y].append(x)
+            else:
+                arr_f[x].append(y)
+            c -= 1
+    return n_f, color_f, arr_f
 
-flag = False
-for i in range(0, n - 1, int(log2(n))+1):
-    if find_cycl_dfs(i):
-        flag = True
-        break
 
-if flag:
-    print("NO")
-else:
-    print("YES")
+def main():
+    n, color, arr = input_gr()
+    flag = False
+    for i in range(n):
+        if color[i] == 0:
+            if find_cycl_dfs(i, color, arr):
+                flag = True
+                break
+    if flag:
+        print("NO")
+    else:
+        print("YES")
+
+
+main()
